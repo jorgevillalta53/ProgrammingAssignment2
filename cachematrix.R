217407e5f3cd5435ac1f781c3cb2132c5f142ab0
Jorge Alberto Villalta <jorge.villalta53@gmail.com>
2014-04-27 07:44:06 -0600

    Update cachematrix.R
    final changes

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,7 @@
 # 1.  makeCacheMatrix: with this function create the special objetcts (i, x),
 #                       make "i" null and create the list of functions
 ##########################################################################################
-makeCacheMatrix <- function(x = matrix()) {   # Uses the "x" argument that will recieve the matrix that it'll use
+makeCacheMatrix <- function(x = matrix()) {# Uses the "x" argument that will recieve the matrix that it'll use
   i<-NULL                                  # makes NULL the "i" special object
   set<-function(y=matrix()){               # Defines the first function that it'll use if you want to set a new matrix
     x<<-y                                  # with the "y" argument it recieves the new matrix and assigns to the special object "x" in the cache
@@ -24,14 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {   # Uses the "x" argument that will 
 ## 2.  cacheSolve: computes the inverse of the special "matrix" (x) OR retrieve the inverse from the cache.
 ##########################################################################################
 
-cacheSolve <- function(x, ...) {            # Uses the argument "list" for recieve the list of function below
+cacheSolve <- function(x, ...) {        # Uses the argument "list" for recieve the list of function below
   inv<-x$getsolve()                     # Assigns to "inv" the value cached of "i" special object (execute getsolve function)
-  if(!is.null(inv)){                       # Verificates if the value it's NULL
-    message ("getting cached data")        # If it's TRUE, shows the message
-    return(inv)                            # Return a matrix that is the inverse of 'x'
-  }                                        # End If... If its FALSE its because "i" doesn't have a value and it has to computes the inverse
+  if(!is.null(inv)){                    # Verificates if the value it's NULL
+    message ("getting cached data")     # If it's TRUE, shows the message
+    return(inv)                         # Return a matrix that is the inverse of 'x'
+  }                                     # End If... If its FALSE its because "i" doesn't have a value and it has to computes the inverse
   data<-x$get()                         # For that, Assigns to "data" the value cached of "x" special object (execute get function)
-  inverse<-solve(data)                     # Compute the inverse matrix (solve) and It store to "inverse" variable
+  inverse<-solve(data)                  # Compute the inverse matrix (solve) and It store to "inverse" variable
   x$setsolve(inverse)                   # with the new value of the inverse matrix, executes the "setsolve" function with "inv" argument for assignt to "i" in the cache
-  inverse                                  # Return a matrix that is the inverse of 'x',that it the same of "i" special object 
+  inverse                               # Return a matrix that is the inverse of 'x',that it the same of "i" special object 
 }
